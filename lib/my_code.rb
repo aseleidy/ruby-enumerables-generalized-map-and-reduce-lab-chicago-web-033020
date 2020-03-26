2c49ec855fb635e76283f289d78e291872883d3a
Ashley Etemadi <ash.etemadi@gmail.com>
2020-03-26 15:57:34 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -17,25 +17,18 @@ map(new) {|n| n * -1}
 
 def reduce(source_array, starting_point = nil)
   if starting_point
+    sum = starting_point
     i = 0 
   else 
+    sum = source_array[0]
     i = 1
-    
-  i = 0 
-  while i < source_array.length do 
-    if source_array[i]
-      true 
-      yield (starting_point += source_array[i])
-    else 
-      return false 
-    end  
-   
-  
-     
-    
-    i += 1
-     
   end 
   
-  starting_point 
+  while i < source_array.length do 
+    sum = yield(sum, source_array[i])
+  
+    i += 1 
+  end
+  
+  sum  
 end 
