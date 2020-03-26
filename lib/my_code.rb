6fe7917f6ea30b50b3dd55ff5262e3653e026f42
Ashley Etemadi <ash.etemadi@gmail.com>
2020-03-26 15:45:00 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -16,6 +16,11 @@ map(new) {|n| n * -1}
 
 
 def reduce(source_array, starting_point = nil)
+  if starting_point
+    i = 0 
+  else 
+    i = 1
+    
   i = 0 
   while i < source_array.length do 
     if source_array[i]
